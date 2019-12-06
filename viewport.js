b33a3dbe90acd906c21e9291d229ae89b3944d31
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2019-12-06 12:42:21 +1100

    Update viewport.js

--- viewport.js
+++ viewport.js
@@ -3,3 +3,10 @@ viewPortTag.id = "viewport";
 viewPortTag.name = "viewport";
 viewPortTag.content = "width=device-width, initial-scale=1";
 document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+var bookOnline = document.getElementById("title_nav").getElementsByClassName("page_heading")[0];
+bookOnline.innerHTML = "Pool Availability";
+
+var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
+facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Facility Booking", "Pool Availbility");
+
