2ee1f6396dbb183cb574fc59c8771e07fc19d790
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-02-14 09:03:09 +1100

    Create ACU.js

---
+++ ACU.js
@@ -0,0 +1,112 @@
+/*MELBOURNE SPORT*/
+var pages = [ 
+    "/https://melbournesport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( "#sso_login_button" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+var pages = [ 
+    "/https://melbournesport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( ".sso_button_container" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+var pages = [ 
+    "/https://melbournesport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( ".sso_legal_text" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+/*BRISBANE SPORT*/
+var pages = [ 
+    "/https://brisbanesport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( "#sso_login_button" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+var pages = [ 
+    "/https://brisbanesport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( ".sso_button_container" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+var pages = [ 
+    "/https://brisbanesport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( ".sso_legal_text" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+/*SYDNEY SPORT*/
+var pages = [ 
+    "/https://strathfieldsport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( "#sso_login_button" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+var pages = [ 
+    "/https://strathfieldsport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( ".sso_button_container" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+var pages = [ 
+    "/https://northsydneysport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( ".sso_legal_text" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+/*STRATHFIELD*/
+
+var pages = [ 
+    "/https://strathfieldsport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( "#sso_login_button" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+var pages = [ 
+    "/https://northsydneysport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( ".sso_button_container" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
+
+var pages = [ 
+    "/https://strathfieldsport.acu.edu.au/public/session/",
+    "https://secure.activecarrot.com/public/session/"
+];
+
+$( ".sso_legal_text" ).toggle(
+    $.inArray( location.pathname, pages ) >= 0
+);
