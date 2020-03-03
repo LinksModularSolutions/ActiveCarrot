74cd32be9db93f48954ab7879863e5f7c6606b85
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-03-03 17:34:50 +1100

    Update launcestonac.js

--- launcestonac.js
+++ launcestonac.js
@@ -1,5 +1,11 @@
-var bookOnline = document.getElementById("title_nav").getElementsByClassName("page_heading")[0];
+var pages = [ 
+    "https://secure.activecarrot.com/customer/booking/facility_read_only/1529",
+  "https://secure.activecarrot.com/customer/booking/facility_read_only/",
+];
+
+  var bookOnline = document.getElementById("title_nav").getElementsByClassName("page_heading")[0];
 bookOnline.innerHTML = "Pool Availability";
 
 var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
 facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Facility Booking", "Pool Availbility");
+
