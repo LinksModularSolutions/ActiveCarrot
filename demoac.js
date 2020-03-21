806847191f779239d63f653113a28885fa7fe3f6
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-03-21 16:51:54 +1100

    Create demoac.js

---
+++ demoac.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("floatright");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.width = "50%";
+  }
+}
