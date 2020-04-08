cbcc5c8ed5c64c962f1b1ecb3334c294fe69562a
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-04-09 01:11:08 +1000

    Update launcestonac.js

--- launcestonac.js
+++ launcestonac.js
@@ -1,8 +1,4 @@
 if (location.pathname.indexOf('customer/dashboard') === -1) {
-  var bookOnline = document.getElementById("title_nav").getElementsByClassName("page_heading")[0];
-  bookOnline.innerHTML = "Pool";
-}
-
-  
   var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
   facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Facility Booking", "Pool Availbility");
+}
