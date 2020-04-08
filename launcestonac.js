a73f3a9846c4ac02d7b5f51a579d7cf1b0726624
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-04-08 14:44:02 +1000

    Create launcestonac.js

---
+++ launcestonac.js
@@ -0,0 +1,7 @@
+if (location.pathname.indexOf('customer/dashboard', 'customer/profile', 'customer/account', 'customer/contract/application', 'customer/class/student_profiles', 'customer/class/browse', 'customer/class/bookings') === -1) {
+  var bookOnline = document.getElementById("title_nav").getElementsByClassName("page_heading")[0];
+  bookOnline.innerHTML = "Pool Availability";
+  
+  var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
+  facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Facility Booking", "Pool Availbility");
+}
