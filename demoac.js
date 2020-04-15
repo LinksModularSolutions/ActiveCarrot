c1b5898a69232954a8e617c2d216afd6a54834a3
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-04-15 13:46:28 +1000

    Delete demoac.js

--- demoac.js
+++
@@ -1,5 +0,0 @@
-var viewPortTag = document.createElement('meta');
-viewPortTag.id = "viewport";
-viewPortTag.name = "viewport";
-viewPortTag.content = "width=device-width, initial-scale=1";
-document.getElementsByTagName('head')[0].appendChild(viewPortTag);
