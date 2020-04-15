b3bf5480e7d93647b82fe0aebf453144a55bb7fa
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-04-15 13:27:09 +1000

    Update demoac.js

--- demoac.js
+++ demoac.js
@@ -3,16 +3,3 @@ viewPortTag.id = "viewport";
 viewPortTag.name = "viewport";
 viewPortTag.content = "width=device-width, initial-scale=1";
 document.getElementsByTagName('head')[0].appendChild(viewPortTag);
-
-<script>
-  function myFunction() {
-  var x = document.getElementById("floatright");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.width = "50%";
-  }
-}
-
-</script>
-
