25e51b61f3d738c7cd46e219c441de137167097d
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-05-18 12:10:06 +1000

    Update PortHedland.js

--- PortHedland.js
+++ PortHedland.js
@@ -1,12 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144561479-4"></script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-144561479-4');
-
-
 // adding meta tag to header
 var metaTag = document.createElement('meta');
 metaTag.name = "robots";
