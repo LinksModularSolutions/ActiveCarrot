0734d300ed551cdda365bca323a57d493db5e381
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-05-18 12:23:39 +1000

    Update PortHedland.js

--- PortHedland.js
+++ PortHedland.js
@@ -1,5 +1,11 @@
 // adding meta tag to header
-var metaTag = document.createElement('meta');
-metaTag.name = "robots";
-metaTag.content = "noindex";
-document.getElementsByTagName('head')[0].appendChild(metaTag);
+var viewPortTag = document.createElement('meta');
+viewPortTag.name = "robots";
+viewPortTag.content = "noindex";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+// adding google analytics script tag to header
+var googleAnalyticsTag = document.createElement('script');
+googleAnalyticsTag.id = "ganalytics";
+googleAnalyticsTag.src = "https://www.googletagmanager.com/gtag/js?id=UA-144561479-4";
+document.getElementsById('ganalytics')[0].appendChild(googleAnalyticsTag);
