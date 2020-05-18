aeeb3cd04fb3f3a419e62bb0d4bf25dd60165c52
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-05-18 12:01:19 +1000

    Create PortHedland.js

---
+++ PortHedland.js
@@ -0,0 +1,7 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144561479-4"></script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-144561479-4');
