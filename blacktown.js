ababf5c25bda3847f0294c847524f4c94ea50f4d
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-06-12 10:13:58 +1000

    Create blacktown.js

---
+++ blacktown.js
@@ -0,0 +1,4 @@
+if (location.pathname.indexOf('customer/dashboard') === -1) {
+  var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
+  facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Group Fitness", "Session");
+}
