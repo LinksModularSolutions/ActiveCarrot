2de302d77df352b06df762f53e5da5d85ddba9d4
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-06-12 10:19:54 +1000

    Update blacktown.js

--- blacktown.js
+++ blacktown.js
@@ -1,9 +1,5 @@
 if (location.pathname.indexOf('customer/dashboard') === -1) {
   var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
   facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Group Fitness", "Session");
-}
-
-if (location.pathname.indexOf('customer/dashboard') === -1) {
-  var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
   facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Facility Booking", "Session");
 }
