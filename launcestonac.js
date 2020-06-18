adf44972776878e270e0cdc8f98b0809ed1567c3
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-06-18 13:41:23 +1000

    Update launcestonac.js

--- launcestonac.js
+++ launcestonac.js
@@ -1,4 +1,4 @@
 if (location.pathname.indexOf('customer/dashboard') === -1) {
   var facilityBooking = document.getElementById("main_content").getElementsByClassName("three_quarters")[0].getElementsByTagName("H2")[0];
-  facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Facility Booking", "Pool Availbility");
+  facilityBooking.innerHTML = facilityBooking.innerHTML.replace("Pool Availbility", "Book Online");
 }
