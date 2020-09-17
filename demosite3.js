c3168c7bb3e7daa9a3c1749b658c7a6352047bed
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-09-17 13:21:18 +1000

    Create demosite3.js

---
+++ demosite3.js
@@ -0,0 +1,28 @@
+  
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, initial-scale=1";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+var pageURL = window.location.href;
+
+console.log(pageURL);
+var pageStatus = pageURL.includes("https://secure.activecarrot.com/customer/dashboard");
+console.log(pageURL.includes("https://secure.activecarrot.com/customer/dashboard"));
+
+if(pageStatus == true) {
+   var cssId = 'myCss'; 
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://cdn.jsdelivr.net/gh/LinksModularSolutions/ActiveCarrot/dashboard.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
+ 
+}
