378dbac3890c8af35568eefedeb30430d886bb15
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-09-17 13:16:05 +1000

    Create demoac1.js

---
+++ demoac1.js
@@ -0,0 +1,28 @@
+  
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, initial-scale=1";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+var pageURL = window.location.href;
+
+console.log(pageURL);
+var pageStatus = pageURL.includes("https://secure.activecarrot.com/customer/dashboard");
+console.log(pageURL.includes("https://secure.activecarrot.com/customer/dashboard"));
+
+if(pageStatus == true) {
+   var link = document.createElement('link');  
+  
+        // set the attributes for link element
+           link.rel = 'https://secure.activecarrot.com/customer/dashboard';  
+      
+        link.type = 'text/css'; 
+      
+        link.href = 'https://cdn.jsdelivr.net/gh/LinksModularSolutions/ActiveCarrot/dashboard.css';  
+  
+        // Get HTML head element to append
+        // link element to it
+        document.getElementsByTagName('HEAD')[0].appendChild(link);  
+  
+}
