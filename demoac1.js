17e41ba8215a7c39ed4184b0bc85cfe4abaee444
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-09-17 13:15:17 +1000

    Update demoac1.js

--- demoac1.js
+++ demoac1.js
@@ -4,10 +4,25 @@ viewPortTag.name = "viewport";
 viewPortTag.content = "width=device-width, initial-scale=1";
 document.getElementsByTagName('head')[0].appendChild(viewPortTag);
 
+var pageURL = window.location.href;
 
-var pages = [ 
-    "https://secure.activecarrot.com/customer/dashboard"
-];
+console.log(pageURL);
+var pageStatus = pageURL.includes("https://secure.activecarrot.com/customer/dashboard");
+console.log(pageURL.includes("https://secure.activecarrot.com/customer/dashboard"));
 
-$('head').append('<link rel="dashboardstylesheet" href="https://cdn.jsdelivr.net/gh/LinksModularSolutions/ActiveCarrot/dashboard.css" type="text/css" />');
+if(pageStatus == true) {
+   var link = document.createElement('link');  
+  
+        // set the attributes for link element
+           link.rel = 'https://secure.activecarrot.com/customer/dashboard';  
+      
+        link.type = 'text/css'; 
+      
+        link.href = 'https://cdn.jsdelivr.net/gh/LinksModularSolutions/ActiveCarrot/dashboard.css';  
+  
+        // Get HTML head element to append
+        // link element to it
+        document.getElementsByTagName('HEAD')[0].appendChild(link);  
+  
+}
 
