aa4e54cc3a2edf1664822867c572f94b6db3c499
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-10-21 14:18:35 +1100

    Create viewport1.js

---
+++ viewport1.js
@@ -0,0 +1,6 @@
+  
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
