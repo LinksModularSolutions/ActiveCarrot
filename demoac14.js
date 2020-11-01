d0f6f6cf12d9cfc8a7b88ad502819ddc141d2e6d
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2020-11-01 11:43:00 +1100

    Create demoac14.js

---
+++ demoac14.js
@@ -0,0 +1,51 @@
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+var pageURL = window.location.href;
+
+console.log(pageURL);
+var pageStatus = pageURL.includes("https://secure.activecarrot.com/customer/dashboard");
+console.log(pageURL.includes("https://secure.activecarrot.com/customer/dashboard"));
+
+if(pageStatus === true) {
+   var cssId = 'myCss'; 
+if (!document.getElementById(cssId))
+{
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://cdn.jsdelivr.net/gh/LinksModularSolutions/ActiveCarrot/dashboard1.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
+
+}
+
+$(document).ready(function(){
+
+{$("#LoginForm > button.NewSignup").on('click', function() {
+   $("#admin_wrapper > form").fadeOut();
+   $("#ProspectIframe").fadeIn();
+});}
+
+{$("#LoginForm > button.ExistingLogin").on('click', function() {
+   $("#admin_wrapper > form").fadeIn();
+   $("#ProspectIframe").fadeOut();
+   $("#ProspectIframe").css("visibility", "visible");
+});
+}
+
+    $("#step-1 > div > div.panel-body > p").addClass("memTypeSelector");
+
+{$('.row.contract_row').hide(); 
+$('.memTypeSelector').click(function() { 
+$(this).next('.row.contract_row').slideToggle(500)
+.siblings('.row.contract_row:visible').slideUp(500);
+});
+}
+});
