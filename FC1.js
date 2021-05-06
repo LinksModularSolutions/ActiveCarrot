61a532d2135f51faf1f1969a4df013b5130d42d9
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2021-05-07 08:42:30 +1000

    Create FC1.js

---
+++ FC1.js
@@ -0,0 +1,26 @@
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, initial-scale=1";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+script type="text/javascript">
+//<![CDATA[
+var theForm = document.forms['form1'];
+if (!theForm) {
+    theForm = document.form1;
+}
+function __doPostBack(eventTarget, eventArgument) {
+    if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+        theForm.__EVENTTARGET.value = eventTarget;
+        theForm.__EVENTARGUMENT.value = eventArgument;
+        theForm.submit();
+    }
+}
+//]]>
+</script>
+<script src="/PoolsData.UI/WebResource.axd?d=pynGkmcFUV13He1Qd6_TZJpoVVhIF-ResNdptB1l5hC-N97hWVSnFkQDqS3rbaVq49pZxjVS-eN8KJ-o8NXQNQ2&amp;t=637454464754849868" type="text/javascript"></script>
+
+<script src="/PoolsData.UI/ScriptResource.axd?d=NJmAwtEo3Ipnlaxl6CMhvqekNZa0o9oJOj5hMJLcVBqyxHk2AURltq0cpIqVr6BvagPqZa8QKW0qlMgz_c4_7-DExFU4WTcvD1xxC_b8yElkJbPNhalgpG9e3XSKQ42IeXrrrF4GYR1f_XH3Jyvx57O_maIjGoA2b6kgT4qFs2w1&amp;t=363be08" type="text/javascript"></script>
+
+<script src="/PoolsData.UI/ScriptResource.axd?d=dwY9oWetJoJoVpgL6Zq8OClOBV_KCwM6-qZ6jAPi-Zzs6gGpzO_gG4_a58y4UH8TjFI2k_a0PPRzYy9iXem4O30pdj8ahE7V2nRBIHkdd9Ipfg6nlw9T_XjIlYByAJ6-mmPrW9_P5gbN498zN6SNO_G7Lrkr1B9gjOPHlaHMmH41&amp;t=363be08" type="text/javascript"></script>
