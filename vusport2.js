2e4cb5e702bab1e518c0f74e74f2ef51c0f8abff
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2021-08-27 07:52:14 +1000

    Create vusport2.js

---
+++ vusport2.js
@@ -0,0 +1,10 @@
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, initial-scale=1";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+$("#emergency_primary_phone_select > option[value=home_phone]").removeAttr('selected');
+$("#emergency_primary_phone_select > option[value=mobile_phone]").attr('selected', 'true');
+$("#emergency_primary_mobile_select > option[value=mobile_phone]").removeAttr('selected');
+$("#emergency_primary_mobile_select > option[value=home_phone]").attr('selected', 'true');
