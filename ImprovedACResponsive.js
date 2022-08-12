9bc52a78a077947f23285359fd67b815225aa446
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2022-08-12 12:40:15 +1000

    Create ImprovedACResponsive.js

---
+++ ImprovedACResponsive.js
@@ -0,0 +1,9 @@
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+
+/*DECLARE AT DASHBOARD*/
+$("p:contains('Logged in as:')").addClass("loggedInDetails");
