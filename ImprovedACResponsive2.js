e3a6e95c94b3aade5fc8e6b364f38cb78aef2100
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2022-08-16 12:58:08 +1000

    Create ImprovedACResponsive2.js

---
+++ ImprovedACResponsive2.js
@@ -0,0 +1,20 @@
+var viewPortTag = document.createElement('meta');
+viewPortTag.id = "viewport";
+viewPortTag.name = "viewport";
+viewPortTag.content = "width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0";
+document.getElementsByTagName('head')[0].appendChild(viewPortTag);
+
+/*HIDE PROFILE AT DASHBOARD*/
+if (location.pathname.indexOf('customer/dashboard') === 1) {
+  $("#my_profile_left_content").hide()
+};
+
+/*DECLARE AT DASHBOARD*/
+$("p:contains('Logged in as:')").addClass("loggedInDetails");
+$("div:contains('Last Attendance')").addClass("dashboardContent");
+$("h2:contains('My Account')").addClass("page_heading");
+
+
+/*Change input type at non responsive public memberships*/
+$(".contract_radio").prop('type', 'button');
+$(".contract_radio").val('Sign Up');
