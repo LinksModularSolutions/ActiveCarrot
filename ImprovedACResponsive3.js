a4e90d8a3c1487c37c134f8d8b8b84ee123c332f
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2022-08-16 13:09:02 +1000

    Update and rename ImprovedACResponsive2.js to ImprovedACResponsive3.js

--- ImprovedACResponsive3.js
+++ ImprovedACResponsive3.js
@@ -13,6 +13,16 @@ if (location.pathname.indexOf('customer/dashboard') === 1) {
 $("p:contains('Logged in as:')").addClass("loggedInDetails");
 $("div:contains('Last Attendance')").addClass("dashboardContent");
 $("h2:contains('My Account')").addClass("page_heading");
+$("h2:contains('Online Store')").addClass("page_heading");
+$("h2:contains('Visit passes available for purchase')").addClass("page_heading");
+$("h2:contains('Student Profiles')").addClass("page_heading");
+$("h2:contains('Browse Classes')").addClass("page_heading");
+$("h2:contains('Class Pre-registration')").addClass("page_heading");
+$("h2:contains('Class Progress')").addClass("page_heading");
+$("h2:contains('My Bookings')").addClass("page_heading");
+$("h2:contains('Casual Entries')").addClass("page_heading");
+$("#main_content > div.three_quarters > h2").addClass("page_heading");
+
 
 
 /*Change input type at non responsive public memberships*/
