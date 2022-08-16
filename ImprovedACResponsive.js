a178e419c41c685d06513ade79fe06e90c930189
LinksModularSolutions <54260170+LinksModularSolutions@users.noreply.github.com>
2022-08-16 12:34:03 +1000

    Update ImprovedACResponsive.js

--- ImprovedACResponsive.js
+++ ImprovedACResponsive.js
@@ -4,6 +4,10 @@ viewPortTag.name = "viewport";
 viewPortTag.content = "width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0";
 document.getElementsByTagName('head')[0].appendChild(viewPortTag);
 
+/*HIDE PROFILE AT DASHBOARD*/
+if (location.pathname.indexOf('customer/dashboard') === 1) {
+  $("#my_profile_left_content").hide()
+};
 
 /*DECLARE AT DASHBOARD*/
 $("p:contains('Logged in as:')").addClass("loggedInDetails");
